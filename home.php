753923563ffc4202eac16a0b721905655c868894
evoque <sarmacamera@gmail.com>
2017-06-14 05:11:14 +0000

    initial commit

---
+++ home.php
@@ -0,0 +1,297 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <script src="https://use.fontawesome.com/4ca6133af6.js"></script>
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+      <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Syncopate" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="home.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
+      <link rel="stylesheet" href="timelify/css/timelify.css">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+          <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper red accent-3">
+                    <div class='container'>
+                        <a href="#!" class="brand-logo">stagger.in</a>
+                        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="fa fa-bars" aria-hidden="true"></i></a>
+                        <ul class="right hide-on-med-and-down">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#hiw">How it works</a></li>
+                            <li><a href="#contact">Contact us</a></li>
+                            <li><a href="#login">Login</a></li>
+                        </ul>
+                        <ul class="side-nav red accent-3" id="mobile-demo">
+                            <li><a href="#home" class='white-text'>Home</a></li>
+                            <li><a href="#hiw" class='white-text'>How it works</a></li>
+                            <li><a href="#contact" class='white-text'>Contact us</a></li>
+                            <li><a href="#login" class='white-text'>Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+          </div>  
+            
+            <div class="carousel carousel-slider center" id='home'>
+                <a class="carousel-item" href="#two!"><img src="images/3.jpg"></a>
+                <a class="carousel-item" href="#four!"><img src="images/4.jpg"></a>
+                <a class="carousel-item" href="#four!"><img src="images/5.jpg"></a>
+                <a class="carousel-item" href="#four!"><img src="images/6.jpg"></a>
+            </div>
+       
+       
+        
+                   
+            <div class="section white">
+               <div class="row container">
+                   <h3 class="header">Handmade Gifts are the Best!</h3>
+                   <p class="grey-text text-darken-3 lighten-3">Handmade gifts ideas that you can inexpensively make yourself . These simple handmade gifts can make any crafty person's heart sing with joy!  Choosing the right gift for your loved ones can be difficult, particularly when you're shopping for somebody who seems to have absolutely everything! The last thing you would probably want to do is to spend money on a gift that's going to sit on a shelf unused,or a trinket, your loved one will look at, once in a while but which otherwise serves no purpose.</p>
+               </div>
+            </div>
+            
+            <div class="parallax-container">
+                <div class="parallax"><img src="images/1.jpg"></div>
+            </div>
+            
+            <div class="section white" id='2'>
+               <div class="row container">
+                   <h3 class="header">Another unique way to show your love again...</h3>
+                   <p class="grey-text text-darken-3 lighten-3">When you are gifting From a handmaker you are not just gifting a thing, you are buying a piece of heart, a part of their soul , a moment of someone's life, AND most importantly you are giving the artist a little more time to do something they are passionate about</p>
+                   <p class="grey-text text-darken-3 lighten-3">We craft so hard to tear glitter from your eyes, because creativity is also about putting forth a part of your memories. There are no mistakes found. All you end up is with another unique way to express your love again.</p>
+               </div>
+            </div>
+            
+            <div class="parallax-container">
+                <div class="parallax"><img src="images/2.jpg"></div>
+            </div>
+            
+             <div class="section white" id='hiw'>
+               <div class="row container">
+                  <h3 class="header center-align">It Works!</h3>
+                  <div class='timeline'>
+                    <h2>Step 1</h2>
+                    <ul class="timeline-items">
+                      <li class="is-hidden timeline-item inverted"> <!-- Normal block, positionned to the left -->
+                        <h3>Register as a partner.</h3>
+                        <hr>
+                        <p>Partner with us to show the world the next level of gifting.</p>
+                        <hr>
+                        <a class="waves-effect waves-light btn light-green white-text accent-3">Register</a>
+                      </li>
+                      <li class="is-hidden timeline-item" id='ss'> <!-- Normal block, positionned to the left -->
+                        <img src='images/home.png'>
+                      </li>
+                    </ul>
+                    
+                    <h2>Step 2</h2>
+                    <ul class="timeline-items">
+                      <li class="is-hidden timeline-item inverted"> <!-- Normal block, positionned to the left -->
+                        <h3>Upload your creations!</h3>
+                        <hr>
+                        <p>Your own international market is just a step away. Upload all your gifts and show your talent to the world.</p>
+                        <hr>
+                        <a class="waves-effect waves-light btn light-green white-text accent-3">Store</a>
+                      </li>
+                      <li class="is-hidden timeline-item" id='ss'> <!-- Normal block, positionned to the left -->
+                        <img src='images/store.png'>
+                      </li>
+                    </ul>
+                    
+                    <h2>Step 3</h2>
+                    <ul class="timeline-items">
+                      <li class="is-hidden timeline-item inverted"> <!-- Normal block, positionned to the left -->
+                        <h3>Chat with your buyers.</h3>
+                        <hr>
+                        <p>Know what your customers want from you and get back the pleasure of fulfilling their needs.</p>
+                      </li>
+                      <li class="is-hidden timeline-item" id='ss'> <!-- Normal block, positionned to the left -->
+                        <img src='images/chat.png'>
+                      </li>
+                    </ul>
+                    
+                    <h2>Step 4</h2>
+                    <ul class="timeline-items">
+                      <li class="is-hidden timeline-item inverted"> <!-- Normal block, positionned to the left -->
+                        <h3>Start selling!</h3>
+                        <hr>
+                        <p>You are all set! Receive orders from all over the world.</p>
+                      </li>
+                    </ul>
+                    
+                  </div>
+                  <br/>
+                  <h3 class="header center-align">It's that simple!</h3>
+               </div>
+            </div>
+            
+            
+            <div class="parallax-container">
+                <div class="parallax"><img src="images/7.jpg"></div>
+            </div>
+            
+            <div class="section white" id='login'>
+               <div class="row container">
+                   <h3 class="header">Register with us today.</h3>
+                   <br/>
+                   <br/>
+                     <div class="row">
+                      <form class="col s12" id='reg_form' onsubmit='regdb()'>
+                        <div class="row">
+                          <div class="input-field col s6">
+                            <input placeholder="Andrew" id="first_name" type="text" class="validate">
+                            <label for="first_name">First Name</label>
+                          </div>
+                          <div class="input-field col s6">
+                            <input placeholder="Louis" id="last_name" type="text" class="validate">
+                            <label for="last_name">Last Name</label>
+                          </div>
+                        </div>
+                      <div class="row">
+                        <div class="input-field col s12">
+                          <input placeholder="abc@xyz.com" id="email" type="email" class="validate">
+                          <label for="email">Email</label>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="input-field col s12">
+                          <input id="password" type="password" class="validate">
+                          <label for="password">Password</label>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="input-field col s12">
+                          <input id="add" type="text" class="validate">
+                          <label for="add">Address</label>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="input-field col s12">
+                          <input id="mobile" type="text" class="validate">
+                          <label for="mobile">Mobile Number</label>
+                        </div>
+                      </div>
+                      <div class="row">
+                          <div class="input-field col s6">
+                            <input placeholder="Hyderabad" id="city" type="text" class="validate">
+                            <label for="city">City</label>
+                          </div>
+                          <div class="input-field col s6">
+                            <input placeholder="Maharashtra" id="state" type="text" class="validate">
+                            <label for="state">State</label>
+                          </div>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+                    </form>
+                  </div>
+                   
+               </div>
+            </div>
+            
+           
+        <footer class="page-footer red accent-3" id='contact'>
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">CONTACT US</h5>
+                <p class="grey-text text-lighten-4"><i class="fa fa-phone" aria-hidden="true"></i>  Call us on: 8885279827</p>
+                <p class="grey-text text-lighten-4">Or</p>
+                <p class="grey-text text-lighten-4"><i class="fa fa-envelope" aria-hidden="true"></i>  Write to us at: hello@stagger.in</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Follow us on</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/staggerindia"><i class="fa fa-facebook" aria-hidden="true"></i>  Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><i class="fa fa-twitter" aria-hidden="true"></i>  Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.instagram.com/stagger.in/"><i class="fa fa-instagram" aria-hidden="true"></i>  Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><i class="fa fa-linkedin" aria-hidden="true"></i>  LinkedIn</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2017 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="https://www.facebook.com/Anuroopshannu">Webmaster</a>
+            </div>
+          </div>
+        </footer> 
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
+       
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+      <script src="timelify/js/jquery.js"></script>
+      <script src="timelify/js/jquery.timelify.js"></script>
+      <script src="https://registerdb-fe2d.restdb.io/rest/_jsapi.js"></script>
+      <script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
+      <script type="text/javascript">
+        (function(){
+          emailjs.init("user_5rTUTaSCHSMJrkuiwQWiB");
+        })();
+      </script>
+      <script>
+        function regdb() {
+          var fname = (document.getElementById ('first_name')).value;
+          var lname = (document.getElementById ('last_name')).value;
+          var name = fname.concat(" ",lname)
+          var email = (document.getElementById ('email')).value;
+          var pwd = (document.getElementById ('password')).value;
+          var add = (document.getElementById ('add')).value;
+          var mob = (document.getElementById ('mobile')).value;
+          var city = (document.getElementById ('city')).value;
+          var state = (document.getElementById ('state')).value;
+          var db = new restdb("592aa45c9fecb2d91b864423");
+          var p = new db.seller({"first_name":fname,"last_name":lname,"email":email,"password":pwd,"address":add,"Mobile":mob,"city":city,"state":state});
+          p.save();
+          emailjs.send("default_service","template_DV8bXtqg",{name: "Stagger",fname: fname,lname: name,email: email,mobile: mob, add  : add,city: city,state: state});
+          alert('job done!!!');
+        }
+      </script>
+      <script>
+          $( document ).ready(function(){
+              $(".button-collapse").sideNav();
+              $('.parallax').parallax();
+              $('.carousel.carousel-slider').carousel({fullWidth: true});
+              setInterval(function() {
+                $('.carousel').carousel('next');
+            }, 5000); 
+            $('.timeline').timelify({
+			        animLeft: "fadeInLeft",
+		          animCenter: "fadeInUp",
+		          animRight: "fadeInRight",
+		          animSpeed: 600,
+		          offset: 150
+	           });
+
+                
+        });
+      </script>
+    </body>
+  </html>
